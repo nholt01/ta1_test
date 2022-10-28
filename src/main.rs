c93ba24c6eb8322df2950fe0a2f5db630b8d2eea
Nathan Holt <nathanwayneholt@gmail.com>
2022-10-27 21:03:54 -0400

    purposefully failing

--- src/main.rs
+++ src/main.rs
@@ -24,4 +24,18 @@ mod tests {
         assert_eq!(custom_trip.from, String::from("home location"));
         assert_eq!(custom_trip.to, String::from("work location"));
     }
+
+    #[test]
+    fn purposefully_failing() {
+        // mimicing catching a bug in the code to see if the Github automated CI/CD can pick it up and fail the build
+        let custom_trip = Trip {
+            depart: 2_f32,
+            from: String::from("home location"),
+            to: String::from("work location"),
+        };
+
+        assert_eq!(custom_trip.depart, 2_f32);
+        assert_eq!(custom_trip.from, String::from("home location"));
+        assert_eq!(custom_trip.to, String::from("wrong location"));
+    }
 }
