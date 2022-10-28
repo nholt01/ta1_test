ab2ffbc09c7d7c480d9890bd95fac2ba9242b0d0
Nathan Holt <nathanwayneholt@gmail.com>
2022-10-27 21:00:54 -0400

    adding dummy code to see if CI runner works

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+struct Trip {
+    depart: f32,
+    from: String,
+    to: String,
+}
+
 fn main() {
     println!("Hello, world!");
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_custom() {
+        let custom_trip = Trip {
+            depart: 2_f32,
+            from: String::from("home location"),
+            to: String::from("work location"),
+        };
+
+        assert_eq!(custom_trip.depart, 2_f32);
+        assert_eq!(custom_trip.from, String::from("home location"));
+        assert_eq!(custom_trip.to, String::from("work location"));
+    }
 }
